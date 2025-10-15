4e92f67ba6b5f4f2395227d053febe5c2ef5ab8e
elghulam-byte <ghulamelfirdausy290307@gmail.com>
2025-10-15 11:02:28 +0700

    percobaan 1

---
+++ SiakadFor09.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class SiakadFor09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double nilai, tertinggi = 0, terendah = 100;
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("MASUKKAN NILAI MAHASISWA KE-" + i + ": ");
+            nilai = sc.nextDouble();
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+            }
+        }
+
+        System.out.println("nilai tertinggi: " + tertinggi);
+        System.out.println("nilai terendah: " + terendah);
+    }
+}
