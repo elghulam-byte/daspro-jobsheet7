b989795b671a2c2b91cb2140551c96b4653548e8
elghulam-byte <ghulamelfirdausy290307@gmail.com>
2025-10-15 12:13:52 +0700

    modifikasi pertanyaan percobaan 1

--- SiakadFor09.java
+++ SiakadFor09.java
@@ -3,18 +3,29 @@ public class SiakadFor09 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
+
         for (int i = 1; i <= 10; i++) {
             System.out.print("MASUKKAN NILAI MAHASISWA KE-" + i + ": ");
             nilai = sc.nextDouble();
+
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
 
-        System.out.println("nilai tertinggi: " + tertinggi);
-        System.out.println("nilai terendah: " + terendah);
+        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
     }
 }
