12f50ac568edc575ea807dd7b467d59fad263d54
elghulam-byte <ghulamelfirdausy290307@gmail.com>
2025-10-15 12:40:30 +0700

    modifikasi pertanyaan percobaan 2

--- SiakadWhile09.java
+++ SiakadWhile09.java
@@ -4,7 +4,7 @@ public class SiakadWhile09 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int nilai,jml,i = 0;
+        int nilai, jml, i = 0;
 
         System.out.print("MASUKKAN JUMLAH MAHASISWA: ");
         jml = sc.nextInt();
@@ -20,6 +20,7 @@ public class SiakadWhile09 {
 
             if (nilai > 80 && nilai <= 100) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah A");
+                System.out.println("Bagus, pertahankan nilainya!");
             } else if (nilai > 73 && nilai <= 80) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B+");
             } else if (nilai > 65 && nilai <= 73) {
@@ -35,5 +36,7 @@ public class SiakadWhile09 {
             }
             i++;
         }
+
+        sc.close();
     }
 }
